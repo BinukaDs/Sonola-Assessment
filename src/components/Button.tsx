2c457b8997bc6299818e66c422c44697b9c5fc03
BinukaDs <binukadasunpriya@gmail.com>
2025-10-23 16:12:56 +0530

    feat: add welcome-page with dark mode support

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const Button = ({ children }: { children: React.ReactNode }) => {
+const Button = ({ children, ...props }: { children: React.ReactNode }) => {
     return (
-        <button className='bg-blue-main inset-shadow-gray-100 p-3.5 w-full text-white rounded-xl inset-shadow-sm inner-shadow-inner-shadow-white hover:bg-blue-main/95 transition-all'>
+        <button className='bg-blue-main inset-shadow-gray-100 dark:inset-shadow-[#FFFFFF0D] p-3.5 w-full text-white rounded-xl inset-shadow-sm inner-shadow-inner-shadow-white hover:bg-blue-main/95 transition-all' {...props}>
             {children}
         </button>
     )
